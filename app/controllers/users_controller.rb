6c2816c032eabf4ee187d97617624767894efc3d
Madalena Boavida Guerra <madalena.boavida@icloud.com>
2020-11-10 20:09:32 +0000

    add user routes for CRUD; member; shallow nesting(skills); add basic controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,6 @@
 class UsersController < ApplicationController
-  def show
-  end
-
-  def new
-  end
-
-  def create
-  end
+  before_action :find_user, only: %i[show dashboard hobbies deck requests skillset]
+  def show; end
 
   def edit
   end
@@ -14,6 +8,24 @@ class UsersController < ApplicationController
   def update
   end
 
-  def dashboard
+  def dashboard; end
+
+  def hobbies; end
+
+  def deck; end
+
+  def requests; end
+
+  def skillset; end
+
+  # def chef
+  #   @chef_name = @restaurant.chef_name
+  # end
+
+  private
+
+  def find_user
+    @user = current_user
+    # @restaurant = Restaurant.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,12 @@
 Rails.application.routes.draw do
-  get 'bookings/show'
-  get 'bookings/new'
-  get 'bookings/edit'
-  get 'bookings/update'
-  get 'hobbies/index'
-  get 'hobbies/show'
-  get 'hobbies/find'
-  get 'hobbies/new'
-  get 'hobbies/create'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/dashboard'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, except: %i[index new create] do
+    member do
+      get :dashboard, :hobbies, :deck, :requests, :skillset
+    end
+    resources :skills
+  end
+  # TODO DASHBOARD ROUTES
 end
