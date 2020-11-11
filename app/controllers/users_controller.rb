ed10c750cd218d6502ddd23ae20e1a9de18eb864
Madalena Boavida Guerra <madalena.boavida@icloud.com>
2020-11-11 16:14:13 +0000

    user feature profile + edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,22 @@
 class UsersController < ApplicationController
-  before_action :find_user, only: %i[show dashboard hobbies deck requests skillset]
+  before_action :find_user, only: %i[show hobbies deck requests skillset profile manage update_fields update_resource]
   def show; end
 
-  def edit
+  def manage
+    @user = current_user
+    @skill = Skill.new
   end
 
-  def update
-  end
+  def update_fields
+    @user = current_user
+    # set_task was already triggered by before_action
 
-  def dashboard; end
+    if update_resource(@user, params)
+      redirect_to profile_users_path, notice: "Profile was updated successfully."
+    else
+      render "users/profile"
+    end
+  end
 
   def hobbies; end
 
@@ -18,13 +26,27 @@ class UsersController < ApplicationController
 
   def skillset; end
 
+  def profile; end
+
+  def update_resource(resource, params)
+    if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
+      resource.update_without_password(user_params)
+    else
+      resource.update_with_password(user_params)
+    end
+  end
+
   # def chef
   #   @chef_name = @restaurant.chef_name
   # end
-
   private
 
+  def user_params
+    params.require(:user).permit(:location, :bio, :current_password)
+  end
+
   def find_user
+    # @task = Task.find(params[:id])
     @user = current_user
     # @restaurant = Restaurant.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, except: %i[index new create destroy] do
-    member do
-      get :dashboard, :hobbies, :deck, :requests, :skillset
+    collection do
+      patch "update_fields", to: "users#update_fields"
+      get :hobbies, :deck, :requests, :skillset, :profile, :manage
     end
     resources :skills
   end
