d64d4e8066a80b2699102006981aae4650991625
Madalena Boavida Guerra <madalena.boavida@icloud.com>
2020-11-11 17:03:52 +0000

    nav link and small fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning db...'
+Hobby.destroy_all
+puts 'Creating hobbies...'
+15.times { User.create!( username: Faker::Name.name, email: Faker::Internet.email, location: Faker::Address.city , password: Faker::Alphanumeric.alphanumeric(number: 10))}
+9.times { Hobby.create!( title: Faker::Marketing.buzzwords, description: Faker::Hacker.say_something_smart, level: (1..10).to_a.sample, user_id: (1..10).to_a.sample, occupancy_limit: (1..20).to_a.sample)}
