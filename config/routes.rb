1a8906b574a125d0f34c5b282626216c21b0d12c
Maurizio Ghisolfi <maurizioghisolfi93@gmail.com>
2020-11-11 13:30:11 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,15 @@
 Rails.application.routes.draw do
-  get 'bookings/show'
-  get 'bookings/new'
-  get 'bookings/edit'
-  get 'bookings/update'
-  get 'hobbies/index'
-  get 'hobbies/show'
-  get 'hobbies/find'
-  get 'hobbies/new'
-  get 'hobbies/create'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/dashboard'
+
   devise_for :users
   root to: 'pages#home'
+
+  resources :users, except: %i[index new create destroy] do
+    member do
+      get :dashboard, :hobbies, :deck, :requests, :skillset
+    end
+    resources :skillset
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :hobbies, only: [ :index, :show, :new, :create ]
