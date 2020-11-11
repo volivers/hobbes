f2793c5bff30c9a6b8f3d5af64bf4aa387acfe60
Maurizio Ghisolfi <maurizioghisolfi93@gmail.com>
2020-11-11 17:11:01 +0100

    now we have the form to book an hobby

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,9 +3,17 @@ class BookingsController < ApplicationController
   end
 
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.user_id = current_user.id
+    if @booking.save!
+      redirect_to @booking, notice: 'Yay! 🎉 You made your request.'
+    else
+      render :new
+    end
   end
 
   def edit
@@ -13,4 +21,16 @@ class BookingsController < ApplicationController
 
   def update
   end
+
+private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:time_slot, :hobby_id)
+  end
+
+
 end
