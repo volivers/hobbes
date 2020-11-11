292fabd0c9084ad7f6b5a67556c9543a4993d872
Vasco Oliveira <48258782+volivers@users.noreply.github.com>
2020-11-11 16:25:20 +0000

    refacto

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :users, except: %i[index new create destroy] do
-    member do
-      get :dashboard, :hobbies, :deck, :requests, :skillset
-    end
-    resources :skillset
-  end
+  # resources :users, except: %i[index new create destroy] do
+      # member do
+        # get :dashboard, :hobbies, :deck, :requests, :skillset
+      # end
+      # resources :skillset
+    # end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, except: %i[index new create destroy] do
